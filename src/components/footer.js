a65c471db8c860b8640dc3f454aef2fd613e58f9
azee-rajput <azee.rajput.7@gmail.com>
2019-10-26 15:46:35 +0500

    footer fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Footer extends Component{
     render(){
         return(
-            <div className="row bg-dark p-5 mt-3" style={{color:"white",width:"100%"}}>
+            <div className="row bg-dark p-5 mt-3" style={{margin:"auto",color:"white",width:"100%"}}>
                 <div className="col-sm-6 text-center">
                     <span>This frontend is a mockup of </span> <a href="tasty.co" target="_blank" rel="noopener noreferrer">Tasty.co</a>
                 </div>
